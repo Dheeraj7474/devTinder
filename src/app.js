37c50028e063ba45ffd7b9e27f2b6457cce2f41d
Dheeraj7474 <dherdheeraj@gmail.com>
2024-09-20 03:08:39 -0500

    Dynamic Data api

--- src/app.js
+++ src/app.js
@@ -1,16 +1,24 @@
 const express = require("express");
-const { verifyToken } = require("./utils");
-
+const { connectDB } = require("./config/database");
 const app = express()
+const User = require("./Models/user");
 
-app.listen(3000)
-
-
-
-app.use("/",verifyToken)
-
-app.get("/test",(req,res)=>{
-    res.send("User valid")
+app.use(express.json())
+app.post("/signup",async (req,res)=>{
+    console.log(req.body)
+    const user = new User(req.body)
+    await user.save()
+    res.send("user added")
 })
+
+
+connectDB().then(()=>{
+    console.log("DB connected successfully")
+    app.listen(3000,()=>{
+        console.log("Server listening on port 3000")
+    })
+})
+.catch(()=>console.log("DB connection unsuccessful"))
+
 
 console.log("hi")
